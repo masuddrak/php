61c6f4f0f87ad880f7566cc58fe2919c4f3bdb19
masud <masudranaacb@gmail.com>
2024-03-04 14:37:21 +0600

    array foreach lop

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-$marks=1050;
-// 80 A+
-// 70 A-
-switch($marks){
-    case($marks>=80 && $marks<=100):
-        echo "A+";
-        break;
-    case($marks<80 && $marks>=70):
-        echo "A-";
-        break;
-    case($marks>60 && $marks<70):
-        echo "B+";
-        break;
-  default:
-  echo "invalid ";
-}
+// index array
+$numbers=[5,5,68,6,56];
+echo $numbers[2]."<br>";
+// asociative array
+$person=[
+    "name"=>"sakib",
+    "age"=>"24",
+    "color"=>"white",
+
+];
+print_r($person);
+
+echo $person["age"];
+echo "<ul>";
+foreach($numbers as $number){
+    echo "<li>$number</li>";
+};
+echo "</ul>";
 ?>
